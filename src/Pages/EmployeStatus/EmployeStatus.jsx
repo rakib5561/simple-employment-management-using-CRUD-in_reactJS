2626344ee138913aec124143527daa2499d8e2ae
rakib5561 <mdrakib5561@gmail.com>
2023-11-03 05:07:57 +0600

    emplyment status

--- src/Pages/EmployeStatus/EmployeStatus.jsx
+++ src/Pages/EmployeStatus/EmployeStatus.jsx
@@ -5,7 +5,17 @@ import { Outlet } from 'react-router-dom';
 const EmployeStatus = () => {
     return (
         <div>
-            <h1>Employe management</h1>
+            
+            <div className='employe-wrapper'>
+                <div className='container'>
+                    <h1 className='employe-title'>Current Status</h1>
+
+                    <div className='Add-employer'>
+                    </div>
+
+                </div> 
+            </div>
+
             <Outlet></Outlet>
         </div>
     );
